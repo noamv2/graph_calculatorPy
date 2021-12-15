7e79522d4714affd9e6d4b3fde1f093d904ae667
Yuval sandler <sandler2806@gmai.com>
2021-12-15 18:14:18 +0200

    digraph

--- src/imp/DiGraph.py
+++ src/imp/DiGraph.py
@@ -1,3 +1,4 @@
+import copy
 import json
 from abc import ABC
 
@@ -6,12 +7,19 @@ from Edge import Edge
 from src.GraphInterface import GraphInterface
 
 
+class Container:
+    def __init__(self, node):
+        self.node = node
+        self.outEdges = {}
+        self.inEdges = {}
+
+
 class DiGraph(GraphInterface, ABC):
 
     def __init__(self, file_name):
         self.mc = 0
-        adjList = dict
-        nodes = dict
+        adjList = {}
+        nodes = {}
         if file_name != "":
             with open(file_name, "r") as f:
                 dict2 = json.load(f)
@@ -23,32 +31,87 @@ class DiGraph(GraphInterface, ABC):
                 n_id = nodeDataD["id"]
                 n_pos = nodeDataD["pos"]
                 node = Node(n_id, n_pos)
-                adjList[n_id] = Container(node)
+                adjList[str(n_id)] = Container(node)
                 nodes[n_id] = node
 
             for edgeData in edgesL:
                 edgeDataD = dict(edgeData)
-                src=edgeDataD["src"]
+                src = edgeDataD["src"]
                 dest = edgeDataD["dest"]
-                weight = edgeDataD["weight"]
-                edge=Edge(src,weight,dest)
-                Container(adjList[src]).outEdges[dest]=edge
-                Container(adjList[dest]).inEdges[src] = edge
+                weight = edgeDataD["w"]
+                edge = Edge(src, weight, dest)
+                adjList[str(src)].outEdges[str(dest)] = edge
+                adjList[str(dest)].inEdges[str(src)] = edge
 
+        self.adjList = adjList
+        self.nodes = nodes
 
+    def get_node(self, node_id) -> Node:
+        return self.nodes[node_id]
 
+    def v_size(self) -> int:
+        return len(self.nodes)
 
+    def e_size(self) -> int:
+        size = 0
+        for container in self.adjList.values():
+            size += len(container.outEdges)
 
-        # else:
+        return size
+
+    def get_all_v(self) -> dict:
+        return copy.deepcopy(self.nodes)
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        return copy.deepcopy(self.adjList[str(id1)].inEdges)
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        return copy.deepcopy(self.adjList[str(id1)].outEdges)
+
+    def get_mc(self) -> int:
+        return self.mc
+
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        if id1 in self.nodes and id2 in self.nodes and id2 not in self.adjList[str(id1)].outEdges:
+            edge = Edge(id1, weight, id2)
+            self.adjList[str(id1)].outEdges[str(id2)] = edge
+            self.adjList[str(id2)].inEdges[str(id1)] = edge
+            self.mc += 1
+            return True
+
+        return False
+
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        if node_id not in self.nodes:
+            node = Node(node_id, {pos[0], pos[1], "0.0"})
+            self.adjList[str(node_id)] = Container(node)
+            self.nodes[str(node_id)] = node
+            return True
+
+        return False
+
+    def remove_node(self, node_id: int) -> bool:
+        if node_id in self.nodes:
+            for i in self.adjList[str(node_id)].outEdges:
+                self.remove_edge(node_id, i)
+            for i in self.adjList[str(node_id)].inEdges:
+                self.remove_edge(i, node_id)
+
+            del self.adjList[str(node_id)]
+            del self.nodes[str(node_id)]
+            self.mc += 1
+            return True
+        return False
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        if node_id2 in self.adjList[str(node_id1)].outEdges:
+            del self.adjList[str(node_id1)].outEdges[str(node_id2)]
+            del self.adjList[str(node_id2)].outEdges[str(node_id1)]
+            self.mc += 1
+            return True
+        return False
 
 
 if __name__ == '__main__':
+    d = DiGraph("../../data/A0.json")
     print("")
-    DiGraph("../../data/A0.json")
-
-
-class Container:
-    def __init__(self, node):
-        self.node = node
-        self.outEdges = dict
-        self.inEdges = dict
