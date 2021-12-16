87e17389268d76a4f266003d1c76ecceb9f28abd
noam vanunu <noamv23@gmail.com>
2021-12-16 11:10:11 +0200

    dijkstra fixed

--- src/imp/GraphAlgo.py
+++ src/imp/GraphAlgo.py
@@ -17,47 +17,9 @@ class GraphAlgo(GraphAlgoInterface):
         pass
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        src = id1
-        dest = id2
-        # get the number of nodes from the graph
-        size = self.graph.v_size()
-        nodes = self.graph.get_all_v()
-        # create and initialize distance and prev dicts, we return these as the result
-        distances = {}
-        prev = {}
-        # create a list that keep track of visited nodes
-        visited = {}
 
-        # and a queue based on heap
-        que = []
-        hp.heappush(que, (0, src))
-
-        for k, v in nodes.items():
-            if k != src:
-                visited[k] = False
-                distances[k] = math.inf
-                prev[k] = None
-
-        distances[src] = 0
-
-        while len(que) > 0:
-            # pop the smallest vertex
-            dis, u = hp.heappop(que)
-            visited[u] = True  # mark the node as visited
-
-            # traverse U's neighbours
-            edges = self.graph.all_out_edges_of_node(u)
-
-            for ID, w in edges.items():
-                # print(visited[ID])
-                if not visited[int(ID)]:
-                    altDis = dis + w.get_w()  # compute the distance to U + dis(u,v)
-                    if altDis < distances[int(ID)]:
-                        distances[int(ID)] = altDis
-                        prev[int(ID)] = u
-                        hp.heappush(que, (altDis, int(ID)))  # requeue v with the new priority
-
-        return distances[dest], self.getPath(prev, src, dest)
+        prev, distances = self.dijkstra(id1)
+        return distances[id2], self.getPath(prev, id1, id2)
 
     def plot_graph(self) -> None:
         pass
@@ -137,7 +99,7 @@ class GraphAlgo(GraphAlgoInterface):
 
         return path
 
-    def dijkstra(src: int) -> (dict, dict):
+    def dijkstra(self, src: int) -> (dict, dict):
         # get the number of nodes from the graph
         size = self.graph.v_size()
         nodes = self.graph.get_all_v()
