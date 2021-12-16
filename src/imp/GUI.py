eecf6e6b58c15928d6ddf5a4d5d9781d73432e9d
danaZo <93203695+danaZo@users.noreply.github.com>
2021-12-16 19:32:03 +0200

    gui - added documentation

--- src/imp/GUI.py
+++ src/imp/GUI.py
@@ -1,34 +1,40 @@
 import math
-import string
 import matplotlib.pyplot as plt
 from DiGraph import DiGraph
-import pandas as pd
-import numpy as np
 
+"""
+in this class we draw the graph we receive from a Json file using matplotlib
+"""
 
-#d = DiGraph("../../data/A0.json")
+# Function draw receives a graph a draw it
 
 
 def draw(di: DiGraph):
     nodes = di.get_all_v()  # all the nodes in the graph
     id_nums = []
 
+    # id_nums holds all the id numbers of the nodes in the graph
+    # used to draw the numbers of each node
     for _id in nodes:
         id_nums.append(str(_id))
 
-    x = []
-    y = []
+    x = []  # the x values of the position of every node
+    y = []  # the y values of the position of every node
+
+    # for scaling
     max_x = math.inf * (-1)
     min_x = math.inf
     max_y = math.inf * (-1)
     min_y = math.inf
-    scalelog=0
-    scalelat=0
+
+    # going through all the nodes id values in the graph
     for node in nodes.values():
-        positions = node.get__pos()
-        list_pos = list(positions.split(","))
-        posX = float(list_pos[0])
-        posY = float(list_pos[1])
+        positions = node.get__pos()  # this node's position (in string format)
+        list_pos = list(positions.split(","))  # converting position into a list
+        posX = float(list_pos[0])  # the value at place 0 is x
+        posY = float(list_pos[1])  # the value at place 1 is y
+
+        # scaling
         if max_x < posX:
             max_x = posX
         if max_y < posY:
@@ -38,10 +44,8 @@ def draw(di: DiGraph):
         if min_y > posY:
             min_y = posY
 
-    scalelog = 1 / (max_x - min_x)
-    scalelat = 1 / (max_y - min_y)
-
-
+    scalelog = 1 / (max_x - min_x)  # scaling the x
+    scalelat = 1 / (max_y - min_y)  # scaling the y
 
     for j in nodes.values():
         positions = j.get__pos()
@@ -51,26 +55,22 @@ def draw(di: DiGraph):
         # y axis value list.
         y.append((float(list_pos[1])-min_y)*scalelat)
 
-
-
-        # Draw point based on above x, y axis values.
-
-        # ax.plot(x, y, c='red',  marker='o')
+        # Draw points based on above x, y axis values.
         dots = plt.scatter(x, y, c='red', marker='o', s=10, zorder=3)
 
         # Loop for annotation of all points
         for i in range(len(x)):
             plt.annotate(id_nums[i], (x[i], y[i] + 0.02))
-        counter = 0
 
-        x_s = []
-        y_s = []
-        x_d = []
-        y_d = []
+        counter = 0
+        x_s = []  # x values of source node
+        y_s = []  # y values of source node
+        x_d = []  # x values of dest node
+        y_d = []  # y values of dest node
 
         # plot line between points
         for node in nodes.values():
-            outE = di.all_out_edges_of_node(node.get__id())
+            outE = di.all_out_edges_of_node(node.get__id())  # the edges that goes out of this node
             for edge_out in outE:
 
                 # the x and y of src node
@@ -91,11 +91,10 @@ def draw(di: DiGraph):
                 y_d.append((float(pos_dest[1]) - min_y) * scalelat)
 
                 # add arrow to plot
-                dx = x_d[counter] - x_s[counter]
-                dy = y_d[counter] - y_s[counter]
+                dx = x_d[counter] - x_s[counter]  # distance between x values of dest node and src node
+                dy = y_d[counter] - y_s[counter]  # distance between y values of dest node and src node
                 plt.arrow(x_s[counter], y_s[counter], dx, dy, head_width=0.02, width=.004, length_includes_head=True, facecolor='black')
                 counter += 1
-
 
     plt.show()
 
---
+++ src/imp/harta.py
@@ -0,0 +1,5 @@
+from DiGraph import DiGraph
+from GUI import draw
+
+d = DiGraph("../../data/A0.json")
+draw(d)
