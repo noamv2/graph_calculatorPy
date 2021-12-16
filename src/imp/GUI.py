e61b11a526ec100beb4774872fd161e3bac4f904
Yuval sandler <sandler2806@gmai.com>
2021-12-16 10:31:57 +0200

    update gui

--- src/imp/GUI.py
+++ src/imp/GUI.py
@@ -1,9 +1,9 @@
+import math
 import string
 import matplotlib.pyplot as plt
 from DiGraph import DiGraph
 from matplotlib.widgets import Cursor
 import numpy as np
-
 
 d = DiGraph("../../data/A0.json")
 
@@ -12,14 +12,36 @@ def draw_nodes():
     nodes = d.get_all_v()  # all the nodes in the graph
     x = []
     y = []
+    max_x = math.inf * (-1)
+    min_x = math.inf
+    max_y = math.inf * (-1)
+    min_y = math.inf
+    scalelog=0
+    scalelat=0
+    for node in nodes.values():
+        positions = node.get__pos()
+        list_pos = list(positions.split(","))
+        posX = float(list_pos[0])
+        posY = float(list_pos[1])
+        if max_x < posX:
+            max_x = posX
+        if max_y < posY:
+            max_y = posY
+        if min_x > posX:
+            min_x = posX
+        if min_y > posY:
+            min_y = posY
+
+    scalelog = 1 / (max_x - min_x)
+    scalelat = 1 / (max_y - min_y)
 
     for j in nodes.values():
         positions = j.get__pos()
         list_pos = list(positions.split(","))
         # x axis value list.
-        x.append(list_pos[0])
+        x.append((float(list_pos[0])-min_x)*scalelog)
         # y axis value list.
-        y.append(list_pos[1])
+        y.append((float(list_pos[1])-min_y)*scalelat)
 
     # Draw point based on above x, y axis values.
     fig, ax = plt.subplots(figsize=(8, 6))
@@ -30,11 +52,3 @@ def draw_nodes():
 
 if __name__ == '__main__':
     draw_nodes()
-
-
-
-
-
-
-
-
